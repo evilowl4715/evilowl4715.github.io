40f0bd940ab8826e6cea1e0ccea6ce5ae771ac54
evilowl <evilowl123@yandex.ru>
2020-04-08 07:57:26 +0400

    dodoam new
    dodoam new

--- dodoma/assets/js/main.js
+++ dodoma/assets/js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-  $('.callback').click(function () {
-    $('.modal').css('display', 'flex');
-  });
-  $('.close').click(function () {
-    $('.modal').css('display', 'none');
-  });
   $('.phone').mask('+7 (999) 999-99-99');
+  // $('.date').mask('12.12.1111');
 });
